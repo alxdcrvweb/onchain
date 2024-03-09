71184b6c4fa40d3e15b7177b239893d3a4069377
alexweb3dev0x <alexweb3dev0x@proton.me>
2024-03-09 20:15:06 +0100

    Fix cursor typo and add link to join button in main module.scss and secondScreen.tsx

--- components/main/secondScreen.tsx
+++ components/main/secondScreen.tsx
@@ -8,24 +8,30 @@ const rows = [
   "Having 100+ transactions ",
   "Active badge on Farcaster",
 ];
-const SecondScreen = ({scrollRef}:any) => {
+const SecondScreen = ({ scrollRef }: any) => {
   return (
     <div className={style.second}>
       <div className={style.second__row}>
         <div className={style.second__text}>
           $onchain is an ERC-20 token on Base, launched in March 2024. $onchain
-          aims to support base builders and onchain contributors.
-          If you’re doing things onchain — no matter how small they are — you’re
-          eligible for $onchain airdrop. Even owning a ens or a freemint base
-          NFT matters. Claiming is open until April 1.{" "}
+          aims to support base builders and onchain contributors. If you’re
+          doing things onchain — no matter how small they are — you’re eligible
+          for $onchain airdrop. Even owning a ens or a freemint base NFT
+          matters. Claiming is open until April 1.{" "}
         </div>
-        <div className={style.second__join}>
+        <a
+          href="https://warpcast.com/~/channel/onchaincoin"
+          target="_blank"
+          className={style.second__join}
+        >
           Join <img src="/arrow.svg" />
-        </div>
+        </a>
       </div>
       <div className={style.second__info}>
         <div>
-          <div className={style.second__title} ref={scrollRef}>airdrop SIZE VARIABLES</div>
+          <div className={style.second__title} ref={scrollRef}>
+            airdrop SIZE VARIABLES
+          </div>
           {rows.map((row, i) => (
             <div className={style.second__part} key={i}>
               <span>🔵</span>
